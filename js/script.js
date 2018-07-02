db84b9f2ecc2edec66374fe864a4473ff1088878
paulo-manzone <pmanzonemaia@gmail.com>
2018-07-02 20:18:40 -0300

    funcionando

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $("#calcular").prop("disabled", true);
     $("#limpar").prop("disabled", true);
+    $("#resultado").val("");
+    $("#variaveis").val("2");
+    
 
     //variáveis do problema
     var z, erro, n;
@@ -25,15 +28,18 @@ $(document).ready(function(){
         }
         var tabela = createTable(n);
         $("#tabelaZ").append(tabela);
-        $("#tabelaZ").append("<br><br> ε: <input id='erro' size=1></input>");
+        $("#tabelaZ").append("<br><br> ε: <input id='erro' size=1 value='0.01'></input>");
         $("#calcular").prop("disabled", false);
         $("#limpar").prop("disabled", false);
+
+
     });
 
     //função do botão calcular
     $("#calcular").click(function(){
         z = $("#z").val();
         erro = $("#erro").val();
+        erro*=0.1;
         var inicial = [];
         for(i=1; i<=n; i++){
             inicial[i] = $("#xi"+i+"").val();
@@ -41,10 +47,16 @@ $(document).ready(function(){
         //configura div resultado
         $("#resultado").show();
         $("#resultado").empty();
+        $("#resultado").val("");
         //chama método
         var resultado = metodoGradiente(z,erro,inicial);
-        console.log(resultado);
-        $("#resultado").val("x = " + resultado +" ;");
+        var stringresultado= "X = (";
+        for(i=1; i<=n; i++){
+            stringresultado+= Math.round(resultado[i]/erro)*erro;
+            if(i!=n) stringresultado +=  ", ";
+        }
+        stringresultado+= ")";
+        $("#resultado").val(stringresultado + "\n\n\nCheque o Console (F12) para passo a passo do método");
     });
 
     //==============================================
@@ -54,9 +66,10 @@ $(document).ready(function(){
     function createTable(n){
         var tabela, i;
         tabela = " Z: ";
-        tabela += "<input type='text' size=50 id='z'></input><br>Ponto inicial: <br>";
+        //preenchendo automaticamente com um exemplo
+        tabela += "<input type='text' size=50 id='z' value=' (x1+x2)**2-(x1+x2+2)'></input><br>Ponto inicial: <br>";
         for(i=1; i<=n; i++){
-            tabela += "x" + i + "<input type='text' size=1 id='xi" + i + "'> </input>"; 
+            tabela += "x" + i + "<input type='text'  value='3' size=1 id='xi" + i + "'></input>"; 
         }
         return tabela;
     }
@@ -72,7 +85,7 @@ $(document).ready(function(){
 
     function f1x(z, pontox){
         var h, fdxmenosh, fdxmaish, T;
-        h = 0.0000001;
+        h = 0.00000001;
         T = parseFloat(pontox)-h;
         fdxmenosh = eval(z);
         T = parseFloat(pontox)+h;
@@ -82,10 +95,10 @@ $(document).ready(function(){
 
     function f2x(z, pontox){
         var h, fdxmenos2h, fdx, fdxmais2h, T;
-        h = 0.0000001;
+        h = 0.00000001;
         T = parseFloat(pontox);
         fdx = eval(z);
-        x = parseFloat(pontox)-2*h;
+        T = parseFloat(pontox)-2*h;
         fdxmenos2h = eval(z);
         T = parseFloat(pontox)+2*h;
         fdxmais2h = eval(z);
@@ -107,9 +120,9 @@ $(document).ready(function(){
                     funcao = funcao.replace(regex, "r");
                 }
             }
-            r=x[i]+0.00001;
+            r=x[i]*1+0.00001;
             frmaish = eval(funcao);
-            r=x[i]-0.00001;
+            r=x[i]*1-0.00001;
             frmenosh = eval(funcao);
             doish = 0.00002;
             g[i] = (frmaish - frmenosh)/doish;
@@ -121,32 +134,56 @@ $(document).ready(function(){
     //          Método Gradiente
     //=====================================
     function metodoGradiente(z, erro, inicial){
-        var res=[], x=[], xant=[], k, l, gradiente = [], xilambda=[], funcaomin, lambda;
+        var res=[], x=[], norma, k, l, grad=[], xilambda=[], funcaomin, lambda;
         var regex;
         x=inicial;
-        xant = x + 10; // garantir primeira iteração
-        for(k=1; x-xant < erro; k++){
-            xant = x;
+        console.log(" ==X inicial==");
+        for(l=1; l<=n; l++){
+            console.log("x["+l+"] = " + Math.round(x[l]/erro)*erro);
+        }
+        norma = 100; lambda = 100; // garantir primeira iteração
+        for(k=1; norma>erro && Math.abs(lambda)>(erro*0.0001); k++){
+            //parada por numero de iterações
+            if(k>100) return ["undefined"];
             //calculando gradiente
-            gradiente = gradiente(z, n, x);
+            grad = gradiente(z, n, x);
             //calculando x + grad*lambda
             for(l=1; l<=n; l++){
-                xilambda[l] = gradiente[l] + "*T +"x[l];
+                xilambda[l] = "("+ grad[l] + "*T+" + x[l]+ ")";
             }
 
             //formando uma equação pra ser minimizada
             funcaomin = z;
             for(l=1; l<=n; l++){
                 regex = new RegExp("x"+l+"", "g");
-                funcaomin.replace(regex, xilambda[l]);
+                funcaomin=funcaomin.replace(regex, xilambda[l]);
             }
             //achando lambda que minimize a função
             lambda = newton(funcaomin, 0);
             //somando a X lambda * g
             for(l=1; l<=n; l++){
-                x[l] = x + g[l] * lambda;
+                x[l] = x[l]*1 + grad[l] * lambda;
             }
+            //calculando norma do gradiente
+            norma=0;
+            for(l=1; l<=n; l++){
+                if(Math.abs(grad[l])>norma) norma = Math.abs(grad[l]);
+            }
+
+            //printando estado no log
+            console.log("=======ITERAÇÃO "+k+"========");
+            for(l=1; l<=n; l++){
+                console.log("x["+l+"] = " + Math.round(x[l]/erro)*erro);
+            }
+            console.log("Gradiente:");
+            for(l=1; l<=n; l++){
+                console.log("G["+l+"] = "+ grad[l]);
+            }
+            console.log("lambda = " + lambda);
+            
         }
+        console.log("++++++++++++++++++++++++++++++++");
+        console.log("numero de iterações necessárias: "+ k);
         return x;
     }
 
@@ -160,7 +197,7 @@ $(document).ready(function(){
         fx = f1x(z,xk); //derivada primeira
         ffx = f2x(z,xk); // derivada segunda
         xkm1 = xk - fx/ffx;
-        while(Math.abs(xkm1-xk)>erro && Math.abs(fx) > erro){
+        while(Math.abs(xkm1-xk)>0.00000000000000001*erro && Math.abs(fx) > erro){
             xk = xkm1;
             fx = f1x(z, xk);
             ffx = f2x(z, xk);
